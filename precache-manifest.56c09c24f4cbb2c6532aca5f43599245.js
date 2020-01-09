50c34577e9f1286e9add032fa55643ff9ecdabfe
Tushar Jain <tusharlock10@gmail.com>
2020-01-10 01:06:35 +0530

    Updates

---
+++ precache-manifest.56c09c24f4cbb2c6532aca5f43599245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fd41a116ab4bea34737b993f95197a",
+    "url": "/geekhouse/index.html"
+  },
+  {
+    "revision": "54a928e00046e9d8a3d9",
+    "url": "/geekhouse/static/js/2.438c933e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/geekhouse/static/js/2.438c933e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "837b08856fb372d87423",
+    "url": "/geekhouse/static/js/main.78ef4b0a.chunk.js"
+  },
+  {
+    "revision": "769952432b3db84be2ce",
+    "url": "/geekhouse/static/js/runtime-main.d08d59a3.js"
+  }
+]);
